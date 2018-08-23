216a38e5d282de344be4c44f314c4e1b78f23969
Wagner Bernardes Teixeira <Wagner@DESKTOP-D8G4ILP>
2018-08-23 01:36:00 -0300

    Adicionado semantic ui para teste

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import CitiesList from './components/citiesList'
 //import Table from './components/table'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+//import 'semantic-ui-css/semantic.min.css';
+
+/*
+ *<MuiThemeProvider>
+          <CitiesList />
+        </MuiThemeProvider>
+  */ 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MuiThemeProvider>
-          <CitiesList />
-        </MuiThemeProvider>
+        <h2>Teste</h2>
       </div>
     );
   }
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,70 +1,55 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import City from './city'
-import {
-    Table,
-    TableBody,
-    TableHeader,
-    TableHeaderColumn,
-    TableRow,
-    TableRowColumn,
-  } from 'material-ui/Table';
+import React from 'react'
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
-class CitiesList extends Component {
-    constructor(props){
-        super(props)
-        this.state = { list : []}
-    } 
-    
-    componentWillMount(){
-        this.getList()
-    }
+const TableExamplePagination = () => (
+  <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Header</Table.HeaderCell>
+        <Table.HeaderCell>Header</Table.HeaderCell>
+        <Table.HeaderCell>Header</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
 
-    getList(){
-        axios.get(`http://computadorcasa.ddns.net:8081/apisignal/dashboard`, {
-            auth: {
-                username: '1234567890',
-                password: '0987654321'
-            }
-        }).then((res) => {            
-            this.setState({ list : res.data || [] });
-        }).catch((e) => {
-            console.log(e)
-            this.setState({ list : [] });
-        });
-    }   
-    renderList(){
-        return this.state.list.map(obj => (
-            <TableRow>
-                <TableRowColumn>{obj.cartorio}</TableRowColumn>
-                <TableRowColumn>{obj.cidade}</TableRowColumn>
-                <TableRowColumn>{obj.estado}</TableRowColumn>
-            </TableRow>
-        ));
-    };
-    
-    render() {
-        const { list } = this.state;
-        let body = []
-        if (list.length > 0) {
-            body = this.renderList();
-        }
-          
-        return(
-        <Table >
-            <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
-            <TableRow>
-                <TableHeaderColumn>Cartório</TableHeaderColumn>
-                <TableHeaderColumn>Cidade</TableHeaderColumn>
-                <TableHeaderColumn>Estado</TableHeaderColumn>
-            </TableRow>
-            </TableHeader>
-            <TableBody displayRowCheckbox={false}>
-                {body}
-            </TableBody>
-            
-        </Table>)
-    }
-}
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>
+          <Label ribbon>First</Label>
+        </Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+      </Table.Row>
+      <Table.Row>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+        <Table.Cell>Cell</Table.Cell>
+      </Table.Row>
+    </Table.Body>
 
-export default CitiesList
+    <Table.Footer>
+      <Table.Row>
+        <Table.HeaderCell colSpan='3'>
+          <Menu floated='right' pagination>
+            <Menu.Item as='a' icon>
+              <Icon name='chevron left' />
+            </Menu.Item>
+            <Menu.Item as='a'>1</Menu.Item>
+            <Menu.Item as='a'>2</Menu.Item>
+            <Menu.Item as='a'>3</Menu.Item>
+            <Menu.Item as='a'>4</Menu.Item>
+            <Menu.Item as='a' icon>
+              <Icon name='chevron right' />
+            </Menu.Item>
+          </Menu>
+        </Table.HeaderCell>
+      </Table.Row>
+    </Table.Footer>
+  </Table>
+)
+
+export default TableExamplePagination
